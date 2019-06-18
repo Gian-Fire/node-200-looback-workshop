cb58580a5a1a96a1ffaa4c5630a99bdf1dd814fa
GianFire <gian.delprado94@gmail.com>
2019-06-18 16:49:19 -0700

    finished project, tests pass

---
+++ test/server.spec.js
@@ -0,0 +1,52 @@
+const server = require('../server/server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+'use strict';
+
+chai.use(chaiHttp);
+
+// server.listen(4444);
+
+describe('loopback Workshop', function () {
+  this.timeout(5000);
+  beforeEach((done) => {
+
+    done();
+  });
+
+  afterEach((done) => {
+    done();
+  })
+
+  it('responds to /', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('responds to /api/players', (done) => {
+    chai.request(server)
+      .get('/api/players')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('responds to /api/teams', (done) => {
+    chai.request(server)
+      .get('/api/teams')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+})
